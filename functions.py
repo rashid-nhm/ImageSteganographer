cf5dc5d5bcc7922239de1c7e5d5574ed9ec2715f
Rashid Hasan <nhmrashid@gmail.com>
2017-11-09 10:21:14 -0500

    Initial Commit of all py scripts and readme

---
+++ functions.py
@@ -0,0 +1,84 @@
+'''
+Function: int2bin
+Description: Takes an integer number as input, outputs the binary of the number
+Parameters:
+	n <int> : Integer number as input
+Return:
+	x <int> : Binary number of n as integer
+'''
+def int2bin(n):
+	return int(str(bin(n))[2:])
+
+	
+	
+'''
+Function: bin2int
+Description: Takes string/integer as input and outputs the integer of that number
+Parameters:
+	n <int/str> : Number as input, could be string or integer
+Return:
+	x <int> : Integer version of number n as integer
+'''
+def bin2int(n):
+	return int(str(n), 2)
+
+	
+'''
+Function: str2bin
+Parameters:
+	n <str> : String to be converted to binary. ByteArray does have a limit on max
+		string size so an extremely large string will cause an error
+Return:
+	x <str> : String of binary representing the original string, separated by space
+'''
+def str2bin(n, join_by=" "):
+	msg = join_by.join([i[2:] for i in list(map(bin, bytearray(n, "ascii")))])
+	return msg
+
+	
+'''
+Function: bin2str
+Description: Takes a binary string as input, outputs the string of the string
+	Assumes that string will be all together and will read in chunks of 7 bits
+Parameters:
+	n <str> : String input of binary
+Returns:
+	x <str> : String of binary input
+'''
+def bin2str(n):
+	return "".join([chr(int(n[i:i+7], 2)) for i in range(0, len(n), 7)])
+
+	
+
+'''
+Function: complement
+Description: Takes a integer as input, outputs the complement of the number
+		ex: 5 => 101 (binary) => 010 (complement, flip all 1s and 0s) => 2
+Parameters:
+	n <int> : Integer as input
+Return:
+	x <int> : Complement of input as integer
+'''
+def complement(n):
+	return n^1
+
+'''
+Function: progress_bar
+Description: Prints a progress bar for visual indication of progress
+Parameters:
+	cur <int> : How much progress has been made
+	ttl <int> : Total progress to be made
+	max_len <int> : Maximum length of the progress bar
+Return:
+	None
+	function prints progress bar to screen
+'''
+from math import ceil
+def progress_bar(cur, ttl, max_len):
+	percentage = (cur / ttl) * 100
+	per_bar = 100 // max_len
+	print("\r|%s%s| %g%%" %(
+		"="*(int(percentage)//per_bar),
+		" "*(max_len - int(percentage)//per_bar),
+		ceil(percentage)
+	), end='\r')
